0868cb875e411233e08bbe6ef23c6366f5d94108
Anne <saigonmafia228@gmail.com>
2019-03-01 17:27:12 -0800

    more codes added

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,8 +1,10 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public enum Grade
     {
-        A,B,C,D,F
+        A, B, C, D, F
     }
 
     public class Enrollment
@@ -10,6 +12,7 @@
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
